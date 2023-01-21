05582389ce06f34a3aa0725d83a96981b03dfc20
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2023-01-21 12:37:22 +0300

    Header component has been created.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,12 @@
+import Header from '../../components/header'
 import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <>
+      <Header />
+    </>
+  )
 }
 
 export default Home

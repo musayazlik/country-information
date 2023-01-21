317789d7083ba62e409f139027a113f829a1da81
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2023-01-21 16:58:39 +0300

    Class arrangements made.

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -7,7 +7,7 @@ const Search = () => {
       <div className='relative w-full'>
         <input
           type='text'
-          className='w-full max-w-md h-12 pl-16 pr-6 py-3 rounded-md shadow-dm-blue/5 shadow-md focus:outline-none focus:ring-2 focus:ring-vdb-lm-text dark:focus:ring-white text-vdb-lm-text dark:bg-vdb-dm-bg dark:shadow-dm-blue/20 dark:text-white '
+          className='w-full max-w-md h-12 pl-16 pr-6 py-4 rounded-md shadow-dm-blue/5 shadow-cs focus:outline-none focus:ring-2 focus:ring-vdb-dm-bg/50 dark:focus:ring-white text-vdb-lm-text dark:bg-vdb-dm-bg dark:shadow-dm-blue/20 dark:text-white '
           placeholder='Search for a country...'
         />
         <div className='absolute top-0 bottom-0 my-auto left-8  flex items-center'>

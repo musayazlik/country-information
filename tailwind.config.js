89460f958db4cd4b3bda10083c88a5a68109ebad
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2023-01-21 12:34:30 +0300

    Color and container variables added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{html,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: '1.5rem',
+      },
+    },
+    colors: {
+      // vdb => very dark blue, dm => dark mode, lm => light mode, vlm => very light mode
+      'dm-blue': '#0a192f',
+      'vdb-dm-bg': 'hsl(207, 26%, 17%)',
+      'vdb-lm-text': 'hsl(200, 15%, 8%)',
+      'dm-gray': 'hsl(0, 0%, 52%)',
+      'vlm-gray': 'hsl(0, 0%, 98%) ',
+      white: 'hsl(0, 0%, 100%)',
+    },
   },
+
   plugins: [],
 }

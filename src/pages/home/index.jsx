9fc41b6d07b6583c9856c13de65a5d0c3320aa44
Musa Yazlık <musayazlik1997@gmail.com>
2023-01-22 11:43:59 +0300

    filteredCountries and clean code fix.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,31 +1,34 @@
 // @ts-nocheck
-import Header from '../../components/header'
+
 import React from 'react'
+import axios from 'axios'
+import { useSelector, useDispatch } from 'react-redux'
+
+/** Country Slice */
+import { setCountries } from '../../store/countrySlice'
+
+/** Components */
+import Header from '../../components/header'
+import Card from '../../components/card'
 import Search from '../../components/search'
 import FilterDropDown from '../../components/filterDropDown'
-import Card from '../../components/card'
-import { useSelector, useDispatch } from 'react-redux'
-import axios from 'axios'
-import { setCountries } from '../../store/countrySlice'
-import { setPerPage } from '../../store/paginationSlice'
 import Pagination from '../../components/pagination'
 
 const Home = () => {
   const dispatch = useDispatch()
-
+  /** Data */
   React.useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_API_URL}/all`)
       .then((res) => {
         dispatch(setCountries(res.data))
-        dispatch(setPerPage(res.data.length))
       })
       .then((err) => {
         console.log(err)
       })
   }, [])
 
-  const data = useSelector((state) => state?.country?.allCountries)
+  const data = useSelector((state) => state?.country)
   const pagination = useSelector((state) => state?.pagination)
 
   return (
@@ -38,23 +41,41 @@ const Home = () => {
       </div>
 
       <div className='container flex flex-col sm:grid grid-cols-12 gap-x-8 gap-y-10 mb-20'>
-        {data
-          .slice(
-            (pagination.currentPage - 1) * pagination.pageShowLength,
-            pagination.pageShowLength * pagination.currentPage
-          )
-          .map((country, index) => {
-            return (
-              <Card
-                key={index}
-                name={country.name.common}
-                population={country.population}
-                region={country.region}
-                capital={country.capital}
-                flag={country.flags.svg}
-              />
-            )
-          })}
+        {data.filteredCountries.length > 0
+          ? data.filteredCountries
+              .slice(
+                (pagination.currentPage - 1) * pagination.pageShowLength,
+                pagination.pageShowLength * pagination.currentPage
+              )
+              .map((country, index) => {
+                return (
+                  <Card
+                    key={index}
+                    name={country.name.common}
+                    population={country.population}
+                    region={country.region}
+                    capital={country.capital}
+                    flag={country.flags.svg}
+                  />
+                )
+              })
+          : data.allCountries
+              .slice(
+                (pagination.currentPage - 1) * pagination.pageShowLength,
+                pagination.pageShowLength * pagination.currentPage
+              )
+              .map((country, index) => {
+                return (
+                  <Card
+                    key={index}
+                    name={country.name.common}
+                    population={country.population}
+                    region={country.region}
+                    capital={country.capital}
+                    flag={country.flags.svg}
+                  />
+                )
+              })}
       </div>
       <Pagination />
     </>

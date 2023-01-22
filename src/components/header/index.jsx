5a1f273432fb5d2f2b6b11714a80b02404602349
Musa Yazlık <musayazlik1997@gmail.com>
2023-01-22 22:44:15 +0300

    Route link update.

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 /** Icons */
 import { RiMoonLine, RiSunLine } from 'react-icons/ri'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   /** Dark Mode State */
@@ -32,9 +33,12 @@ const Header = () => {
     <header className='bg-white py-6 shadow-cs dark:shadow-dm-blue/50 dark:bg-vdb-dm-bg duration-200 '>
       <div className='container'>
         <div className='flex flex-cols justify-between items-center'>
-          <h1 className='font-extrabold text-sm sm:text-xl text-vdb-lm-text dark:text-white'>
-            Where in the world?
-          </h1>
+          <Link to={'/'}>
+            <h1 className='font-extrabold text-sm sm:text-xl text-vdb-lm-text dark:text-white'>
+              Where in the world?
+            </h1>
+          </Link>
+
           <div
             onClick={() => darkModeStatus()}
             className='text-vdb-lm-text dark:text-white cursor-pointer font-semibold flex justify-center items-center gap-x-2'>
